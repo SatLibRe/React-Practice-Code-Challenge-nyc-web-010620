951aa5d3cfea8e9e0235cca0e40c5b6e616db52c
Miles Higbie <miles.higbie@gmail.com>
2020-03-16 13:26:49 -0400

    4 sushi rendered

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,12 +5,28 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
+
 class App extends Component {
+
+  state = {
+    sushis: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/sushis")
+    .then(response => response.json())
+    .then(response =>{
+      this.setState({
+        sushis: response
+      })
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushis={this.state.sushis.slice(0,4)}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,14 +1,13 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi.js'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+         props.sushis.map(sushi => <Sushi key={sushi.id} sushi={sushi}/>)
         }
         <MoreButton />
       </div>
